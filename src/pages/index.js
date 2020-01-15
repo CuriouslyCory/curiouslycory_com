4e6b0d2448a64a636a10c55030751311a44325af
Cory Sougstad <hexkrak@hexkrak.com>
2020-01-14 22:24:21 -0700

    Change verbage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ import './index.css'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Find me on...</h1>
+    <h1>Look for me on...</h1>
     <ul>
       <li><MyLink link="https://curiouslycory.etsy.com/" logo="Etsy" /></li>
-      <li><MyLink link="https://www.youtube.com/channel/UCASQA6u80u7Py_UHIGFYqVA?view_as=subscriber" logo="Youtube" /></li>
+      <li><MyLink link="https://www.youtube.com/channel/UCASQA6u80u7Py_UHIGFYqVA?view_as=subscriber" logo="YouTube" /></li>
       <li><MyLink link="https://www.instagram.com/curiouslycory" logo="Instagram" /></li>
       <li><MyLink link="https://blog.hau.me" logo="My Blog" /></li>
       <li><MyLink link="https://cory.hau.me" logo="My (Outdated) Resume" /></li>
