dd5feb5f35a135f0cab4646782d85fae87c2b7a4
Cory Sougstad <hexkrak@hexkrak.com>
2020-01-14 20:27:45 -0700

    Basic styles and logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,8 @@ const IndexPage = () => (
     <ul>
       <li><MyLink link="https://curiouslycory.etsy.com/" logo="Etsy" /></li>
       <li><MyLink link="https://www.youtube.com/channel/UCASQA6u80u7Py_UHIGFYqVA?view_as=subscriber" logo="Youtube" /></li>
+      <li><MyLink link="https://blog.hau.me" logo="My Blog" /></li>
+      <li><MyLink link="https://cory.hau.me" logo="My (Outdated) Resume" /></li>
     </ul>
   </Layout>
 )
