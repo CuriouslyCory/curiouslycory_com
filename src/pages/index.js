40b53a4d62d4d5016255d98b4fa0205de99c6733
Cory Sougstad <hexkrak@hexkrak.com>
2020-01-14 20:49:10 -0700

    add instagram and mail

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,10 @@ const IndexPage = () => (
     <ul>
       <li><MyLink link="https://curiouslycory.etsy.com/" logo="Etsy" /></li>
       <li><MyLink link="https://www.youtube.com/channel/UCASQA6u80u7Py_UHIGFYqVA?view_as=subscriber" logo="Youtube" /></li>
+      <li><MyLink link="https://www.instagram.com/curiouslycory" logo="Instagram" /></li>
       <li><MyLink link="https://blog.hau.me" logo="My Blog" /></li>
       <li><MyLink link="https://cory.hau.me" logo="My (Outdated) Resume" /></li>
+      <li><MyLink link="mailto:cory@curiouslycory.com" logo="Email" /></li>
     </ul>
   </Layout>
 )
