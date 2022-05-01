69eae286e776ea4dc4e7804cdd41688b5819bde7
Cory Sougstad <cory@hau.me>
2022-05-01 15:25:59 -0700

    Update fees

--- src/views/pro-services/index.tsx
+++ src/views/pro-services/index.tsx
@@ -7,21 +7,20 @@ export default function ProServices() {
           <Col>
             <h2>Standard Rates</h2>
             <ul>
-              <li>Low Complexity (Basic NFT, ERC-20, frontend) - $95</li>
-              <li>High Complexity (Solidity Development) - $130</li>
+              <li>Hourly Development - $130</li>
               <li>Consultation and Coaching - $150</li>
             </ul>
 
             <h2>Packages</h2>
             <ul>
-              <li>Basic NFT Pack - $300 (plus contract deployment gas)</li>
+              <li>Basic NFT Pack - $350 (plus contract deployment fees)</li>
               <ul>
                 <li>Minting Site</li>
                 <li>Github configuration</li>
                 <li>Hosting up to 100GB/mo</li>
                 <li>Contract Deployment</li>
               </ul>
-              <li>Basic Token - $200 (plus contract deployment gas)</li>
+              <li>Basic Token - $250 (plus contract deployment fees)</li>
               <ul>
                 <li>Token Site</li>
                 <li>Github configuration</li>
