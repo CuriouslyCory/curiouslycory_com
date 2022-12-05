6b7da29e7956ee991cdd08220b9bc3fe92c8e35f
Cory Sougstad <cory@hau.me>
2022-12-05 12:27:02 -0700

    Revert "Pull out unused next-auth"
    This reverts commit ef60c9189142eb94c5ce89fd70471914bacc67b7.

--- prisma/migrations/20221205185231_base_migration/migration.sql
+++
@@ -1,4 +0,0 @@
--- CreateTable
-CREATE TABLE "Example" (
-    "id" TEXT NOT NULL PRIMARY KEY
-);
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,11 +1,14 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
 import superjson from "superjson";
-import { baseRouter } from "./base-router";
+
+import { exampleRouter } from "./example";
+import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("baseRouter", baseRouter);
+  .merge("example.", exampleRouter)
+  .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
