43e44390f032b611bb725b3de5255d18602452c1
Cory Sougstad <cory@hau.me>
2022-12-12 15:47:34 -0700

    Add featured tech and more services

--- src/features/home-page/services.tsx
+++ src/features/home-page/services.tsx
@@ -5,18 +5,27 @@ export const Services = (): JSX.Element => {
     <section className="w-full mt-10 md:mt-32 p-10 mb-10 md:mb-40 bg-opacity-10 flex justify-center">
       <div className="container flex flex-col md:flex-row items-center">
         <div className="flex-none">
-          <h1 className="text-5xl pb-5 hl-underline">
+          <h2 className="text-5xl pb-5 hl-underline">
             Some of the services we provide
-          </h1>
+          </h2>
           <ul>
+            <li>React Development</li>
+            <li>Angular Development</li>
             <li>Web3</li>
             <li>Solidity</li>
             <li>NFTs &amp; Minting Sites</li>
-            <li>Crypto Tokens</li>
-            <li>Project Forks</li>
+            <li>ERC20 Tokens</li>
+            <li>New Websites</li>
+            <li>Website Updates</li>
+            <li>App Development</li>
+            <li>ServiceNow</li>
             <li>Coaching</li>
             <li>Consultation</li>
             <li>Project Managment</li>
+            <li>
+              Don&apos;t see what you&apos;re looking for?{" "}
+              <CalendlyLink>Let&apos;s chat!</CalendlyLink>
+            </li>
           </ul>
         </div>
         <div className="flex-1 flex justify-center">
