89fa971c1f058f95c49bb5ffeb59d520fa4f7408
Cory Sougstad <cory@hau.me>
2022-12-16 20:35:34 -0700

    New Yarn Cache. Fix some formatting on blog.

--- src/features/blog/components/blog-post-full.tsx
+++ src/features/blog/components/blog-post-full.tsx
@@ -1,33 +1,12 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { Block, BLOCKS, Inline, INLINES } from "@contentful/rich-text-types";
+import { Block, BLOCKS, Inline } from "@contentful/rich-text-types";
 import { Entry } from "contentful";
 import Image from "next/image";
-import { ReactChild, ReactFragment, ReactPortal } from "react";
+import { ReactNode } from "react";
 import { BlogPost } from "../types/contentful";
 
-const options = {
-  renderText: (text: string) => {
-    return text
-      .split("\n")
-      .reduce((children: any, textSegment: any, index: number) => {
-        return [...children, index > 0 && <br key={index} />, textSegment];
-      }, []);
-  },
+const documentOptions = {
   renderNode: {
-    [INLINES.ENTRY_HYPERLINK]: (
-      node: Block | Inline,
-      children:
-        | boolean
-        | ReactChild
-        | ReactFragment
-        | ReactPortal
-        | null
-        | undefined
-    ) => (
-      <a href={`/blog/${node.data["target"].fields.slug}`}>
-        {`${(node.content[0] as any)["value"].toString()}`}
-      </a>
-    ),
     [BLOCKS.EMBEDDED_ASSET]: (node: any) => (
       <Image
         src={`https:${node.data?.target?.fields?.file?.url}`}
@@ -37,6 +16,12 @@ const options = {
         width={node.data?.target?.fields?.file.details.image.width ?? 100}
       />
     ),
+    [BLOCKS.HEADING_2]: (node: Block | Inline, children: ReactNode) => (
+      <h2 className="text-4xl pb-4">{children}</h2>
+    ),
+    [BLOCKS.PARAGRAPH]: (node: Block | Inline, children: ReactNode) => (
+      <p className="pb-4">{children}</p>
+    ),
   },
 };
 
@@ -44,8 +29,8 @@ type BlogPostProps = { blogPost: Entry<BlogPost> };
 
 export const BlogPostFull = ({ blogPost }: BlogPostProps) => {
   return (
-    <div className="block">
-      <div>{documentToReactComponents(blogPost.fields.body, options)}</div>
+    <div className="block w-full">
+      {documentToReactComponents(blogPost.fields.body, documentOptions)}
     </div>
   );
 };
