80376508bdad76dc412b4c8146433c04d58a9a01
Cory Sougstad <cory@hau.me>
2022-12-16 22:54:40 -0700

    Move routes into constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,22 @@
+export const routes = [
+  {
+    title: "Home",
+    path: "/",
+  } as const,
+  {
+    title: "Services",
+    path: "/services",
+  } as const,
+  {
+    title: "About",
+    path: "/about",
+  } as const,
+  {
+    title: "Blog",
+    path: "/blog",
+  } as const,
+  {
+    title: "Contact Us",
+    path: "/contact-us",
+  } as const,
+] as const;
