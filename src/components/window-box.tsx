6adcf1124ad0b382e63e4217be321ad3fb7e5c34
Cory Sougstad <cory@hau.me>
2022-12-16 22:20:48 -0700

    Add optional padding to window-box

--- src/components/window-box.tsx
+++ src/components/window-box.tsx
@@ -1,11 +1,21 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 type WindowBoxProps = {
   children: ReactNode;
+  padding?: string;
 };
-export const WindowBox = ({ children }: WindowBoxProps): JSX.Element => {
+export const WindowBox = ({
+  padding,
+  children,
+}: WindowBoxProps): JSX.Element => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center text-center bg-slate-700 p-10 bg-opacity-10 shadow-md">
+    <div
+      className={clsx(
+        "w-full h-full flex flex-col justify-center items-center text-center bg-slate-700 bg-opacity-10 shadow-md",
+        padding ?? " p-10"
+      )}
+    >
       {children}
     </div>
   );
