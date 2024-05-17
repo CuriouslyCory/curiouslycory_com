4e562a3bbf9d5b1fc7fc2b3fdb29dca7b4b13468
Cory Sougstad <cory@hau.me>
2024-05-17 20:00:00 +0000

    Add services and about page

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,62 +1,57 @@
+import clsx from "clsx";
+import Image from "next/image";
 import Link from "next/link";
-import style from "./header.css";
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import styles from "./header.module.css";
 
 export const Header = (): JSX.Element => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <div id="header-wrapper" className="px-2 sm:px-4 py-2.5">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a>
+        <Link href="/" className="flex items-center">
+          <Image src="/images/cc-logo.svg" width="100" height="120" />
+        </Link>
         <button
-          data-collapse-toggle="navbar-default"
+          onClick={() => setNavOpen(!navOpen)}
           type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
           <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          <IoMenu className="text-3xl" />
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={clsx(
+            { hidden: !navOpen },
+            "w-full",
+            "md:block",
+            "md:w-auto"
+          )}
+          id="navbar-default"
+        >
           <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
             <li>
-              <Link href="/" className="nav-link">
-                Home
+              <Link href="/">
+                <span className={styles.navLink}>Home</span>
               </Link>
             </li>
             <li>
-              <Link href="/services" className="nav-link">
-                Services
+              <Link href="/services" className={styles.navLink}>
+                <span className={styles.navLink}>Services</span>
               </Link>
             </li>
             <li>
-              <Link href="/about" className="nav-link">
-                About
+              <Link href="/about" className={styles.navLink}>
+                <span className={styles.navLink}>About</span>
               </Link>
             </li>
             <li>
-              <Link href="/find-me" className="nav-link">
-                Find Me At...
+              <Link href="/find-me" className={styles.navLink}>
+                <span className={styles.navLink}>Find Me At...</span>
               </Link>
             </li>
           </ul>
