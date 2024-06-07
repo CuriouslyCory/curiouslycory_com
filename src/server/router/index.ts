7b85b60e1ba03b10bf4b3b8d805e585de423ddad
Cory Sougstad <cory@hau.me>
2024-06-07 00:00:00 +0000

    Pull out unused next-auth

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,14 +1,11 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
 import superjson from "superjson";
-
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
+import { baseRouter } from "./base-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
+  .merge("baseRouter", baseRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
