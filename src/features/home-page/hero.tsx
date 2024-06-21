952e0f42b591f213e7da2cc8df78556ddb7b4b04
Cory Sougstad <cory@hau.me>
2024-06-21 20:00:00 +0000

    Fix alignment issues

--- src/features/home-page/hero.tsx
+++ src/features/home-page/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const HomeHero = (): JSX.Element => {
   return (
     <section className="w-full hero-bg pt-20 pb-20">
-      <div className="container flex flex-col-reverse md:flex-row justify-between items-center mt-10 md:mt-20 mb-20 mx-2 md:mx-10">
+      <div className="container flex flex-col-reverse md:flex-row justify-between items-center mt-10 md:mt-20 mb-20 mx-2 md:mx-20">
         <div className="mb-20 bg-[#f7f5f2]/80 p-5 shadow-lg">
           <h1 className="text-5xl pb-5">
             Web Development <span className="hero-underline">Simplified</span>
