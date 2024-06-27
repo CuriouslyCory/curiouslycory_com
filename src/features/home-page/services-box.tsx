e7a23f55ec9af9b42d05f954785119769dbf8314
Cory Sougstad <cory@hau.me>
2024-06-27 20:00:00 +0000

    Make height consistent regardless of content.

--- src/features/home-page/services-box.tsx
+++ src/features/home-page/services-box.tsx
@@ -11,7 +11,7 @@ const ServicesBox = () => {
         Full Stack and More
       </h2>
       <div className="grid grid-rows-4 md:grid-rows-none grid-cols-none md:grid-cols-4 justify-between items-center gap-5">
-        <div className="w-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
+        <div className="w-full h-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
           <FaLaptopCode size={64} className="mb-4 text-gray-600" />
           <h3 className="text-xl font-semibold text-gray-800 mb-4">
             Web Development
@@ -21,7 +21,7 @@ const ServicesBox = () => {
             is tailored to your business needs.
           </p>
         </div>
-        <div className="w-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
+        <div className="w-full h-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
           <GiChaingun size={64} className="mb-4 text-gray-600" />
           <h3 className="text-xl font-semibold text-gray-800 mb-4">
             Web 3 and Blockchain
@@ -31,7 +31,7 @@ const ServicesBox = () => {
             into the blockchain.
           </p>
         </div>
-        <div className="w-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
+        <div className="w-full h-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
           <FaMobileAlt size={64} className="mb-4 text-gray-600" />
           <h3 className="text-xl font-semibold text-gray-800 mb-4">
             Mobile App Development
@@ -41,7 +41,7 @@ const ServicesBox = () => {
             your business&apos;s reach and engagement.
           </p>
         </div>
-        <div className="w-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
+        <div className="w-full h-full flex flex-col justify-center items-center text-center px-4 py-6 bg-slate-700 p-10 bg-opacity-10 shadow-md">
           <IoIosPeople size={64} className="mb-4 text-gray-600" />
           <h3 className="text-xl font-semibold text-gray-800 mb-4">
             Consulting Services
