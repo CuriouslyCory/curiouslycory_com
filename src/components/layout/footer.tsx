f2718bb55d29d15a1bbba9663ae55516a715dfce
Cory Sougstad <cory@hau.me>
2024-07-24 16:00:00 +0000

    Fix formatting issue. Correct socail links. Use routes instead of hardcoded links.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,65 +1,38 @@
 import Link from "next/link";
 import React from "react";
+import { FaTwitter } from "react-icons/fa";
+import { routes } from "../../constants/routes";
+import { socialLinks } from "../../constants/socials";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <div className="mx-2 md:mx-20 flex flex-col md:flex-row justify-between items-center">
         <p className="text-sm font-semibold mb-3 md:mb-0">
           Copyright CuriouslyCory {new Date().getFullYear()}
         </p>
-        <ul className="flex items-center mb-6 md:mb-0">
-          <li className="mr-3">
-            <Link href="/">
-              <button className="inline-block text-gray-600 hover:text-white">
-                Home
-              </button>
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/services">
-              <button className="inline-block text-gray-600 hover:text-white">
-                Services
-              </button>
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/about">
-              <button className="inline-block text-gray-600 hover:text-white">
-                About
-              </button>
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/contact-us">
-              <button className="inline-block text-gray-600 hover:text-white">
-                Contact Us
-              </button>
-            </Link>
-          </li>
-        </ul>
-        <ul className="flex items-center">
-          <li className="mr-3">
-            <a href="#">
-              <i className="fab fa-twitter fa-lg"></i>
-            </a>
-          </li>
-          <li className="mr-3">
-            <a href="#">
-              <i className="fab fa-facebook-f fa-lg"></i>
-            </a>
-          </li>
-          <li className="mr-3">
-            <a href="#">
-              <i className="fab fa-instagram fa-lg"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-linkedin-in fa-lg"></i>
-            </a>
-          </li>
-        </ul>
+        <div className="flex flex-col gap-5">
+          <ul className="flex items-center mb-6 md:mb-0">
+            {routes.map((route) => (
+              <li key={`footer-route-${route.title}`} className="mr-3">
+                <Link href={route.path}>
+                  <button className="inline-block text-gray-600 hover:text-white">
+                    {route.title}
+                  </button>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex items-center justify-end">
+            {socialLinks.map((social) => (
+              <li className="mr-3" key={`footer-social-${social.title}`}>
+                <a href={social.href}>
+                  <social.Icon />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   );
