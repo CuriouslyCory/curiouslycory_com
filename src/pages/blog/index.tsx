f52c95aee31018cc7ffd009471a1f6ef70f50745
Cory Sougstad <cory@hau.me>
2024-08-13 20:00:00 +0000

    Removing the blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,10 +1,6 @@
 import { SectionTitle } from "../../components/section-title";
-import { BlogPostPreview } from "../../features/blog/components/blog-post-preview";
-import { trpc } from "../../utils/trpc";
 
 export const BlogPage = (): JSX.Element => {
-  const { data: posts } = trpc.contentful.getBlogPosts.useQuery();
-
   return (
     <main className="flex flex-col justify-center items-center">
       <div className="hidden md:block fixed inset-x-0 top-0 -z-10">
@@ -12,14 +8,6 @@ export const BlogPage = (): JSX.Element => {
       </div>
       <section className="mx-2 md:mx-20 md:flex-row justify-between items-center mt-10 md:mt-20">
         <SectionTitle size="5xl">Featured Posts</SectionTitle>
-        <div className="grid grid-rows-3 md:grid-rows-none grid-cols-none md:grid-cols-3 justify-between items-center gap-5 mb-20">
-          {posts?.map((post) => (
-            <BlogPostPreview
-              key={`post-${post.fields.title}`}
-              blogPost={post}
-            />
-          ))}
-        </div>
       </section>
     </main>
   );
