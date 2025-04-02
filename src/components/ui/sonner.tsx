df07a9df52afeb4fcf6de7466424aced59ca1426
Cory Sougstad <cory@hau.me>
2025-04-01 17:19:53 -0700

    Updated shadcn components for v4

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,34 +1,25 @@
-"use client";
+"use client"
 
-import { useTheme } from "next-themes";
-import { Toaster as Sonner } from "sonner";
-
-type ToasterProps = React.ComponentProps<typeof Sonner>;
+import { useTheme } from "next-themes"
+import { Toaster as Sonner, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+  const { theme = "system" } = useTheme()
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      toastOptions={{
-        classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg text-lg grid grid-cols-6",
-          content: "col-span-5 mb-2",
-          title: "text-xl",
-          description: "group-[.toast]:text-muted-foreground ",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground col-span-3 w-full font-semibold justify-center",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground col-span-3 w-full font-semibold justify-center",
-          closeButton: "text-muted-foreground",
-        },
-      }}
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
       {...props}
     />
-  );
-};
+  )
+}
 
-export { Toaster };
+export { Toaster }
